c9bfe3566a698dcf71968d624ba02446111e02dc
Mohsen shokooh <mohsenshokooh37@gmail.com>
2022-11-06 06:53:30 -0800

    Real Instagram clone. you can create and see profiles

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 instagram Clone Backend
 
+## User:
 
+- [x] Create Account
+- [x] See Profile
+- [ ] Login
+- [ ] Edit Profile
+- [ ] Follow User
+- [ ] Unfollow User
+- [ ] Change Avatar(Image Upload)
---
+++ users/user.mutations.js
@@ -0,0 +1,33 @@
+import client from "../client";
+import bcrypt from "bcrypt";
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, userName, email, password }
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [{ userName }, { email }],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This Username/Email is already Taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            password: uglyPassword,
+          },
+        });
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};
